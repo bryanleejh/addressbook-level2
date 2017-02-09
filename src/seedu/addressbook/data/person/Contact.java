1d63a831ece9f6d617bfa1a8bc5798731617152d
bryanleejh <bryan.lee.jh@gmail.com>
2017-02-09 22:21:34 +0800

    Extract Contact class

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,24 @@
+package seedu.addressbook.data.person;
+
+public abstract class Contact {
+	protected String value;
+	protected boolean isPrivate;
+
+	public String toString() {
+		return value;
+	}
+	
+	public int hashCode() {
+        return value.hashCode();
+    }
+
+	public boolean isPrivate() {
+        return isPrivate;
+    }
+	
+	public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Contact // instanceof handles nulls
+                && this.value.equals(((Contact) other).value)); // state check
+    }
+}
