a094bf8a2b8417ec0b6f9df71b107b4f672e82b6
bryanleejh <bryan.lee.jh@gmail.com>
2017-02-09 22:33:43 +0800

    Adjust formatting of code

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -7,18 +7,19 @@ public abstract class Contact {
 	public String toString() {
 		return value;
 	}
-	
+
 	public int hashCode() {
-        return value.hashCode();
-    }
+		return value.hashCode();
+	}
 
 	public boolean isPrivate() {
-        return isPrivate;
-    }
-	
+		return isPrivate;
+	}
+
 	public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof Contact // instanceof handles nulls
-                && this.value.equals(((Contact) other).value)); // state check
-    }
+		return other == this // short circuit if same object
+				|| (other instanceof Contact // instanceof handles nulls
+						&& this.value.equals(((Contact) other).value)); // state
+																		// check
+	}
 }
--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -5,83 +5,86 @@ import seedu.addressbook.data.tag.UniqueTagList;
 import java.util.Objects;
 
 /**
- * Represents a Person in the address book.
- * Guarantees: details are present and not null, field values are validated.
+ * Represents a Person in the address book. Guarantees: details are present and
+ * not null, field values are validated.
  */
 public class Person implements ReadOnlyPerson {
 
-    private Name name;
-    private Phone phone;
-    private Email email;
-    private Address address;
+	private Name name;
+	private Phone phone;
+	private Email email;
+	private Address address;
 
-    private final UniqueTagList tags;
-    /**
-     * Assumption: Every field must be present and not null.
-     */
-    public Person(Name name, Phone phone, Email email, Address address, UniqueTagList tags) {
-        this.name = name;
-        this.phone = phone;
-        this.email = email;
-        this.address = address;
-        this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
-    }
+	private final UniqueTagList tags;
 
-    /**
-     * Copy constructor.
-     */
-    public Person(ReadOnlyPerson source) {
-        this(source.getName(), source.getPhone(), source.getEmail(), source.getAddress(), source.getTags());
-    }
+	/**
+	 * Assumption: Every field must be present and not null.
+	 */
+	public Person(Name name, Phone phone, Email email, Address address, UniqueTagList tags) {
+		this.name = name;
+		this.phone = phone;
+		this.email = email;
+		this.address = address;
+		this.tags = new UniqueTagList(tags); // protect internal tags from
+												// changes in the arg list
+	}
 
-    @Override
-    public Name getName() {
-        return name;
-    }
+	/**
+	 * Copy constructor.
+	 */
+	public Person(ReadOnlyPerson source) {
+		this(source.getName(), source.getPhone(), source.getEmail(), source.getAddress(), source.getTags());
+	}
 
-    @Override
-    public Phone getPhone() {
-        return phone;
-    }
+	@Override
+	public Name getName() {
+		return name;
+	}
 
-    @Override
-    public Email getEmail() {
-        return email;
-    }
+	@Override
+	public Phone getPhone() {
+		return phone;
+	}
 
-    @Override
-    public Address getAddress() {
-        return address;
-    }
+	@Override
+	public Email getEmail() {
+		return email;
+	}
 
-    @Override
-    public UniqueTagList getTags() {
-        return new UniqueTagList(tags);
-    }
+	@Override
+	public Address getAddress() {
+		return address;
+	}
 
-    /**
-     * Replaces this person's tags with the tags in the argument tag list.
-     */
-    public void setTags(UniqueTagList replacement) {
-        tags.setTags(replacement);
-    }
+	@Override
+	public UniqueTagList getTags() {
+		return new UniqueTagList(tags);
+	}
 
-    @Override
-    public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof ReadOnlyPerson // instanceof handles nulls
-                && this.isSameStateAs((ReadOnlyPerson) other));
-    }
+	/**
+	 * Replaces this person's tags with the tags in the argument tag list.
+	 */
+	public void setTags(UniqueTagList replacement) {
+		tags.setTags(replacement);
+	}
 
-    @Override
-    public int hashCode() {
-        // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, address, tags);
-    }
+	@Override
+	public boolean equals(Object other) {
+		return other == this // short circuit if same object
+				|| (other instanceof ReadOnlyPerson // instanceof handles nulls
+						&& this.isSameStateAs((ReadOnlyPerson) other));
+	}
 
-    @Override
-    public String toString() {
-        return getAsTextShowAll();
-    }
+	@Override
+	public int hashCode() {
+		// use this method for custom fields hashing instead of implementing
+		// your own
+		return Objects.hash(name, phone, email, address, tags);
+	}
+
+	@Override
+	public String toString() {
+		return getAsTextShowAll();
+	}
 
 }
